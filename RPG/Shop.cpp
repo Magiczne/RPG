5a77d00674a6b79b5e782aa630f61059b92c8094
Magiczne <michalkamilkleszczynski@gmail.com>
2017-05-12 14:39:34 +0200

    Shop::proceed implementation

--- RPG/Shop.cpp
+++ RPG/Shop.cpp
@@ -6,11 +6,42 @@ using namespace Items;
 using namespace Encounters;
 
 using namespace System;
+using namespace System::IO;
+using namespace System::Collections::Generic;
 
 Shop::Shop(String^ configFile) 
 	: IShop(configFile)
 {
-	
+	this->_data = File::ReadAllLines(this->ConfigFile);
+}
+
+bool Shop::proceed(ICharacter^ character)
+{
+	auto question = this->_data[0];
+
+	List<String^>^ answers = gcnew List<String^>();
+
+	while(true)
+	{
+		answers->Clear();
+		for each(auto item in this->AvailableItems)
+		{
+			answers->Add(item->Name);
+		}
+		answers->Add(this->_data[1]);
+
+		auto choice = this->UserInterface->askQuestion(question, answers);
+
+		if (choice == answers->Count - 1)
+		{
+			return true;
+		}
+		else
+		{
+			this->SelectedItem = this->AvailableItems[choice];
+			this->trade(character);
+		}
+	}
 }
 
 void Shop::trade(ICharacter^ character)
--- RPG/Shop.h
+++ RPG/Shop.h
@@ -10,6 +10,7 @@ namespace Encounters
 	{
 	private:
 		Items::IItem^ _selectedItem;
+		array<System::String^>^ _data;
 
 	public:
 		Shop(System::String^ configFile);
@@ -19,6 +20,13 @@ namespace Encounters
 		/// </summary>
 		/// <param name="character">Character to trade with</param>
 		virtual void trade(ICharacter^ character) override;
+
+		/// <summary>
+		/// Process an encounter on player
+		/// </summary>
+		/// <param name="character">Character on whom encounter was proceeded</param>
+		/// <returns>Encounter succedeed</returns>
+		virtual bool proceed(ICharacter^ character) override;
 
 		/// <summary>
 		/// Selected Item
